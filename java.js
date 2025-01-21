1329aab377986aafa3ed97c43a8d3d06ec82e837
rana-saboorr <167083735+rana-saboorr@users.noreply.github.com>
2025-01-21 17:22:18 +0500

    Create java.js

---
+++ java.js
@@ -0,0 +1,4 @@
+// JavaScript to handle dynamic text change on button click
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('dynamicText').textContent = "You've clicked the button!";
+});
